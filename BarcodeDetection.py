7c3cdbebb2005aa6dc18dd7a74642af58b3b5741
Roberto Longo <21242423+robertolongo@users.noreply.github.com>
2025-10-14 22:25:21 +0200

    Added directory scan

--- BarcodeDetection.py
+++ BarcodeDetection.py
@@ -1,18 +1,23 @@
 import cv2
 import imutils
 from pyzbar.pyzbar import decode
+import os
 
-IMAGE_IN = r"C:\Users\rober\PycharmProjects\BarcodeDetection\NON_LEGGIBILE_doc09917620250930090745_002.jpg"
+IMAGE_IN = r"C:\Users\rober\PycharmProjects\BarcodeDetection\data_in\NON_LEGGIBILE_doc09836820250923153800_009.jpg"
 #IMAGE_IN = r"C:\Users\rober\PycharmProjects\BarcodeDetection\multi_barcode.jpg"
-IMAGE_OUT = r"C:\Users\rober\PycharmProjects\BarcodeDetection"
-ROTATE_DEG = 1
+IMAGE_OUT = r"C:\Users\rober\PycharmProjects\BarcodeDetection\data_out"
+FOLDER_IN = r"C:\Users\rober\PycharmProjects\BarcodeDetection\data_in"
+ROTATE_DEG = 0
 
-def read_barcode(current_image):
-    # Convert to grayscale
-    #gray = cv2.cvtColor(current_image, cv2.COLOR_BGR2GRAY)
+def read_barcode(current_image, angle):
+
+    barcode_list = []
+
+    # Rotation
+    img = imutils.rotate(current_image, angle=angle)
 
     # Extract the barcodes
-    barcodes = decode(current_image)
+    barcodes = decode(img)
 
     # Loop over detected barcodes
     for barcode in barcodes:
@@ -22,8 +27,11 @@ def read_barcode(current_image):
 
         print("Barcode data:", barcode_data, " type:", barcode_type)
 
-        return barcode_data
-    return "no_barcode_found"
+        barcode_list.append(barcode_data)
+
+
+
+    return barcode_list
 
 #image = cv2.imread(r"C:\Users\rober\PycharmProjects\BarcodeDetection\doc09917620250930090745_002.jpg")
 document_image = cv2.imread(IMAGE_IN)
@@ -39,6 +47,22 @@ document_image = cv2.imread(IMAGE_IN)
 
 image_rotated = imutils.rotate(document_image, angle=ROTATE_DEG)
 #image_rotated = imutils.rotate(image_rotated, angle=1)
-barcode_data = read_barcode(image_rotated)
-cv2.imwrite(IMAGE_OUT + r"\\" + barcode_data + ".jpg",image_rotated)
+barcode_data = read_barcode(document_image, 0)
+#cv2.imwrite(IMAGE_OUT + r"\\" + barcode_data[0] + ".jpg",image_rotated)
+
+
+directory = os.fsencode(FOLDER_IN)
+
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    full_filename = os.path.join(FOLDER_IN, filename)
+    print(full_filename)
+    if filename.endswith(".jpg") or filename.endswith(".jpeg"):
+        document_image = cv2.imread(full_filename)
+
+        barcode_data = read_barcode(document_image, 0)
+
+    else:
+        print('Not a valid image')
+
 
